ca7d686f05faeb23b051cd8e1065186efd8a673b
mdhvince <vinceslas.medhy@gmail.com>
2023-05-07 21:13:47 +0200

    Add fixture

--- tests/planning/test_minimum_snap.py
+++ tests/planning/test_minimum_snap.py
@@ -4,6 +4,29 @@ import pytest
 from planning.minimum_snap import MinimumSnap
 
 
+@pytest.fixture
+def waypoints():
+    waypoints = np.array([[10., 0.0, 1.0],
+                          [10., 4.0, 1.0],
+                          [6.0, 5.0, 1.5],
+                          [4.0, 7.0, 1.5],
+                          [2.0, 7.0, 2.0],
+                          [1.0, 0.0, 2.0]])
+    return waypoints
+
+
+@pytest.fixture
+def coord_obstacles():
+    coord_obstacles = np.array([[8.0, 6.0, 1.5, 5.0, 0.0],
+                                [4.0, 9.0, 1.5, 5.0, 0.0],
+                                [4.0, 1.0, 2.0, 5.0, 0.0],
+                                [3.0, 5.0, 1.0, 5.0, 0.0],
+                                [4.0, 3.5, 2.5, 5.0, 0.0],
+                                [5.0, 5.0, 10., 0.5, 5.0]])
+    return coord_obstacles
+
+
+
 def test_insert_midpoints_at_indexes():
     # Arrange
     points = np.array([
