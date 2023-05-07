5a04d027bef71d75fde2233da7b22e56a35b7aea
mdhvince <vinceslas.medhy@gmail.com>
2023-05-07 16:27:12 +0200

    Add tests for trajectory with no obstacles

--- tests/planning/test_minimum_snap.py
+++ tests/planning/test_minimum_snap.py
@@ -36,3 +36,41 @@ def test_insert_midpoints_at_indexes_with_empty_indexes():
     assert result == pytest.approx(expected)
 
 
+def test_is_collision():
+    # Arrange
+    points = np.array([1, 1, 1])
+    obstacles = np.array([[1, 1, 1], [2, 2, 2], [3, 3, 3]])
+    expected = True
+
+    # Act
+    result = MinimumSnap.is_collision(points, obstacles)
+
+    # Assert
+    assert result == expected
+
+
+def test_is_collision_is_false():
+    # Arrange
+    points = np.array([.5, .5, .5])
+    obstacles = np.array([[1, 1, 1], [2, 2, 2], [3, 3, 3]])
+    expected = False
+
+    # Act
+    result = MinimumSnap.is_collision(points, obstacles)
+
+    # Assert
+    assert result == expected
+
+
+def test_is_collision_with_empty_obstacles():
+    # Arrange
+    points = np.array([.5, .5, .5])
+    obstacles = np.array([])
+    expected = False
+
+    # Act
+    result = MinimumSnap.is_collision(points, obstacles)
+
+    # Assert
+    assert result == expected
+
