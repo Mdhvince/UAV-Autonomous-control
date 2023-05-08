08b3a840e4d9a66b4cec214f5dd92a21bb9d7e93
mdhvince <vinceslas.medhy@gmail.com>
2023-05-08 02:22:54 +0200

    test for polynom

--- tests/planning/test_minimum_snap.py
+++ tests/planning/test_minimum_snap.py
@@ -101,7 +101,8 @@ def test_generate_time_per_spline(waypoints):
     # Arrange
     T = MinimumSnap(waypoints, velocity=1.0, dt=0.02)
     T.nb_splines = waypoints.shape[0] - 1
-    distances = np.array([1.0, 3.0, 2.0])
+
+    distances = np.array([1.0, 3.0, 2.0])  # computed manually from waypoints fixture
     expected = distances / T.velocity
 
     # Act
@@ -111,3 +112,43 @@ def test_generate_time_per_spline(waypoints):
     assert T.times == pytest.approx(expected)
 
 
+@pytest.mark.parametrize("order, expected", [
+    (0, np.array([1., 0., 0., 0., 0., 0., 0., 0.])),
+    (1, np.array([0., 1., 0., 0., 0., 0., 0., 0.])),
+    (2, np.array([0., 0., 2., 0., 0., 0., 0., 0.])),
+    (3, np.array([0., 0., 0., 6., 0., 0., 0., 0.])),
+    (4, np.array([0., 0., 0., 0., 24., 0., 0., 0.])),
+    (5, np.array([0., 0., 0., 0., 0., 120., 0., 0.])),
+    (6, np.array([0., 0., 0., 0., 0., 0., 720., 0.])),
+])
+def test_polynom_at_t0(order, expected):
+    # Arrange
+    n_coeffs = 8
+    t = 0
+
+    # Act
+    result = MinimumSnap.polynom(n_coeffs, order, t)
+
+    # Assert
+    assert result == pytest.approx(expected)
+
+
+@pytest.mark.parametrize("order, expected", [
+    (0, np.array([1., 3., 9., 27., 81., 243., 729., 2187.])),
+    (1, np.array([0., 1., 6., 27., 108., 405., 1458., 5103.])),
+    (2, np.array([0., 0., 2., 18., 108., 540., 2430., 10206.])),
+    (3, np.array([0., 0., 0., 6., 72., 540., 3240., 17010.])),
+    (4, np.array([0., 0., 0., 0., 24., 360., 3240., 22680.])),
+    (5, np.array([0., 0., 0., 0., 0., 120., 2160., 22680.])),
+    (6, np.array([0., 0., 0., 0., 0., 0., 720., 15120.])),
+])
+def test_polynom_at_t3(order, expected):
+    # Arrange
+    n_coeffs = 8
+    t = 3
+
+    # Act
+    result = MinimumSnap.polynom(n_coeffs, order, t)
+
+    # Assert
+    assert result == pytest.approx(expected)
